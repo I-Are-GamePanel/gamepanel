437050ada2b20c9a1fc7099d8a30784cacabe0a3
Frank Harris <iaretechnician@gmail.com>
2020-12-21 06:00:50 -0500

    Added description of what this file does
    This file is just for reference on using wget to retrieve a folder

--- scripts/wget_folders.sh
+++ scripts/wget_folders.sh
@@ -1,1 +1,9 @@
+ #so we dont forget how to do something
+ #howto get an entire folder with wget
+ #omitting the index.html files and not
+ #recreating the directory structure.
+ #the key here is --cut-dirs=2 which goes
+ #back 2 folder levels removing the 
+ #postinstall/dayzmod
+ 
  sudo wget -r -np -nH --cut-dirs=2  -R "index.html*" files.iaregamer.com/postinstall/dayzmod/
